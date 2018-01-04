705cf4924165f7c8c1ee60cfec32381d9a8837ef
Pratham Gupta <prathamgupta567@gmail.com>
2018-01-04 20:19:52 +0530

    Supplementary files
    Additional files (Temporary) used to check various devices.

---
+++ time_difference/time_difference.ino
@@ -0,0 +1,14 @@
+void setup(){}
+
+
+void loop()
+  {
+   unsigned long StartTime = millis();
+
+delay(5000);
+
+unsigned long CurrentTime = millis();
+unsigned long ElapsedTime = CurrentTime - StartTime;
+Serial.print (ElapsedTime);
+  }
+
